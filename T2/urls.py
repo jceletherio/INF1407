d015648628496f319bcb53bac5178a6c6675b356
jceletherio <eletherio09@gmail.com>
2020-11-24 17:06:45 -0300

    24/11/2020

--- T2/urls.py
+++ T2/urls.py
@@ -13,13 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeDoneView, PasswordChangeView
+from django.urls.base import reverse_lazy
 from mainapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='homepage'),
     path('SegundaPagina', views.segundaPagina, name='segunda'),
+    path('accounts/', views.homeSec, name='sec-home'),
+    path('accounts/registro/', views.registro, name='sec-registro'),
+    path('accounts/login/', LoginView.as_view(template_name='mainapp/registro/login.html',), name='sec-login'),
+    path('accounts/profile/', views.paginaSecreta,name='sec-paginaSecreta'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('sec-home'),), name='sec-logout'),
+    path('accounts/password_change/', PasswordChangeView.as_view(template_name='mainapp/registro/trocaSenha.html', success_url=reverse_lazy('sec-password_change_done'),), name='sec-password_change'),
+    path('accounts/password_change_done/', PasswordChangeDoneView.as_view(template_name='mainapp/registro/trocaFeita.html',), name='sec-password_change_done'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
     
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -8,3 +9,29 @@ def home(request):
 
 def segundaPagina(request):
     return render(request, 'mainapp/segunda.html')
+
+def homeSec(request):
+    return render(request, 'mainapp/registro/homeSec.html')
+
+def registro(request):
+ if request.method == 'POST':
+    formulario = UserCreationForm(request.POST)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('sec-home')
+ else:
+    formulario = UserCreationForm()
+    context = {'form': formulario,}
+    return render(request, 'mainapp/registro/registro.html', context)
+
+def login(request):
+    return render(request, 'mainapp/registro/login.html')
+
+def paginaSecreta(request):
+    return render(request, 'mainapp/registro/paginaSecreta.html')
+
+def trocaSenha(request):
+    return render(request, 'mainapp/registro/trocaSenha.html')
+    
+def trocaFeita(request):
+    return render(request, 'mainapp/registro/trocaFeita.html')
